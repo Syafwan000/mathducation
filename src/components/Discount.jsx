3c6d62c15c35f2c894a58f53e32458c770a4695e
Syafwan000 <muhammadsyafwan04@gmail.com>
2022-03-25 11:03:32 +0700

    discount & exponent

--- src/components/Discount.jsx
+++ src/components/Discount.jsx
@@ -1,22 +1,60 @@
-import { Container, InputGroup, FormControl } from 'react-bootstrap';
+import { Container, InputGroup, Row, Col } from 'react-bootstrap';
+import NumberFormat from 'react-number-format';
+import { useState } from 'react';
 import Helmet from 'react-helmet';
 import './Discount.css';
 
 function Discount() {
+    const [price, setPrice] = useState('');
+    const [discount, setDiscount] = useState('');
+
+    const result1 = price.replace(/,/g,'') * discount / 100;
+    const result2 = price.replace(/,/g,'') - (price.replace(/,/g,'') * discount / 100);
+
     return (
         <>
             <Helmet>
                 <title>Mathducation | Discount</title>
             </Helmet>
-            <Container className="my-4 text-center">
+            <Container className="my-5 text-center">
                 <h1 className="title-discount">Discount</h1>
                 <p className="title-discount-text mt-3">Discounts are one of the most powerful marketing strategies<br /> to attract consumers to buy products or services offered by sellers.</p>
             </Container>
-            <Container>
-                <InputGroup>
+            <Container className="discount-wrapper mt-3">
+                <InputGroup className="d-flex justify-content-center">
                     <InputGroup.Text id="basic-addon1">Rp</InputGroup.Text>
-                    <FormControl placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" />
+                    <NumberFormat className="discount-input1" placeholder="Price" aria-label="Price" aria-describedby="basic-addon1" thousandSeparator={true} allowNegative={false} onChange={
+                        function(e) {
+                            setPrice(e.target.value);
+                        }
+                    } />
                 </InputGroup>
+                <InputGroup className="mt-4 d-flex justify-content-center">
+                    <NumberFormat className="discount-input2" placeholder="Discount" aria-label="Discount" aria-describedby="basic-addon2" allowNegative={false} onChange={
+                        function(e) {
+                            setDiscount(e.target.value);
+                        }
+                    } />
+                    <InputGroup.Text id="basic-addon2">%</InputGroup.Text>
+                </InputGroup>
+                <Container>
+                    <Row>
+                        <Col>
+                            <h4 className="discount-info text-center mt-3">Discount</h4>
+                            <Container className="d-flex justify-content-center">
+                                <NumberFormat className="discount-result text-center" thousandSeparator={true} value={result1} prefix={'Rp. '} disabled />
+                            </Container>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <h4 className="discount-info text-center mt-3">Price After Discount</h4>
+                            <Container className="d-flex justify-content-center">
+                                <NumberFormat className="discount-result text-center" thousandSeparator={true} value={result2} prefix={'Rp. '} disabled />
+                            </Container>
+                        </Col>
+                    </Row>
+                </Container>
             </Container>
         </>
     )
--- src/components/Exponent.jsx
+++ src/components/Exponent.jsx
@@ -1,15 +1,45 @@
-import { Container } from 'react-bootstrap';
+import { Container, InputGroup } from 'react-bootstrap';
+import NumberFormat from 'react-number-format';
 import Helmet from 'react-helmet';
+import { useState } from 'react';
 import './Exponent.css';
 
 function Exponent() {
+    const [number, setNumber] = useState('');
+    const [exponent, setExponent] = useState('');
+
+    const result =  number.replace(/,/g,'') ** exponent;
+
     return (
         <>
             <Helmet>
                 <title>Mathducation | Exponent</title>
             </Helmet>
-            <Container className="my-4 text-center">
+            <Container className="my-5 text-center">
                 <h1 className="title-exponent">Exponent</h1>
+                <p className="title-exponent-text mt-3">Exponent in mathematics can be interpreted as<br /> repetition of the number itself.</p>
+            </Container>
+            <Container className="exponent-wrapper mt-3">
+                <InputGroup className="d-flex justify-content-center">
+                    <NumberFormat className="exponent-input1" placeholder="Number" aria-label="Number" aria-describedby="basic-addon3" thousandSeparator={true} allowNegative={false} onChange={
+                            function(e) {
+                                setNumber(e.target.value);
+                            }
+                    } />
+                </InputGroup>
+                <InputGroup className="mt-4 d-flex justify-content-center">
+                    <NumberFormat className="exponent-input2" placeholder="Exponent" aria-label="Exponent" aria-describedby="basic-addon4" allowNegative={false} onChange={
+                            function(e) {
+                                setExponent(e.target.value);
+                            }
+                    } />
+                </InputGroup>
+                <Container>
+                    <h4 className="exponent-info text-center mt-3">Result</h4>
+                    <Container className="d-flex justify-content-center">
+                        <NumberFormat className="exponent-result text-center" thousandSeparator={true} value={result} disabled />
+                    </Container>
+                </Container>
             </Container>
         </>
     )
